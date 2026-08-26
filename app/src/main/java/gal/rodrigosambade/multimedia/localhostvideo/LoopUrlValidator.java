6f57d4ca4c4f12f28a973d01afee16539e04d70d
Rodrigo Sambade <rodrigosambadesaa@gmail.com>
2026-08-26 13:14:41 +0200

    Make URL validator JVM-testable

--- app/src/main/java/gal/rodrigosambade/multimedia/localhostvideo/LoopUrlValidator.java
+++ app/src/main/java/gal/rodrigosambade/multimedia/localhostvideo/LoopUrlValidator.java
@@ -1,14 +1,23 @@
 package gal.rodrigosambade.multimedia.localhostvideo;
 
-import android.net.Uri;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 final class LoopUrlValidator {
     private LoopUrlValidator() {}
 
     static boolean isEmulatorHostUrl(String value) {
-        Uri uri = Uri.parse(value);
-        String scheme = uri.getScheme();
-        return ("http".equals(scheme) || "https".equals(scheme))
-                && "10.0.2.2".equals(uri.getHost());
+        if (value == null) {
+            return false;
+        }
+
+        try {
+            URI uri = new URI(value);
+            String scheme = uri.getScheme();
+            return ("http".equalsIgnoreCase(scheme) || "https".equalsIgnoreCase(scheme))
+                    && "10.0.2.2".equals(uri.getHost());
+        } catch (URISyntaxException ignored) {
+            return false;
+        }
     }
 }
